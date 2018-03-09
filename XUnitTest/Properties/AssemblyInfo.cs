ab41ac59326c09f305d696323190de97d464e0bc
vinay <vinaykumarbhandari@gmail.com>
2018-03-09 14:31:01 +0530

    added xUnitTest

---
+++ XUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3e981077-0aa7-41f9-9aa1-eab90a387908")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ XUnitTest/XUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+using Trending;
+namespace XUnitTest
+{
+
+    public class XUnitTest
+    {
+        [Fact]
+        public void Test_Trending_XUnitTest_OK()
+        {
+            var result = TrendingRunner.WhatsTrending(1);
+            Assert.Equal("Paul Walker", result);
+
+            result = TrendingRunner.WhatsTrending(2);
+            Assert.Equal("Cory Monteith", result);
+
+            result = TrendingRunner.WhatsTrending(2);
+            Assert.Equal("Cory Monteith", result);
+
+            result = TrendingRunner.WhatsTrending(3);
+            Assert.Equal("RoyalBaby", result);
+
+        }
+
+        [Fact]
+        public void Test_Trending_XUnitTest_FAIL()
+        {
+            var result = TrendingRunner.WhatsTrending(1);
+            Assert.Equal("Paul Walker", result);
+        }
+    }
+}
+
