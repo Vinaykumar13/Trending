38da145d1094f157f2a70b95bf01da11550c64f1
Vinaykumar13 <vinaykumarbhandari@gmail.com>
2018-04-09 14:15:46 +0530

    Update Nunit Tests.cs

--- NUnitTest/Nunit Tests.cs
+++ NUnitTest/Nunit Tests.cs
@@ -17,27 +17,27 @@ namespace NUnitTest
             result.Should().Be("Paul Walker");
         }
 
-        //[TestCase]
-        //public void Test_Trending_NUnit_Fluent_FAIL()
-        //{
-        //    Console.Out.WriteLine("Hi from Nunit");
+        [TestCase]
+        public void Test_Trending_NUnit_Fluent_FAIL()
+        {
+            Console.Out.WriteLine("Hi from Nunit");
 
-        //    var result = TrendingRunner.WhatsTrending(1);
-        //    result.Should().Be("Paul Talker"); // This should fail, typo.
-        //}
+            var result = TrendingRunner.WhatsTrending(1);
+            result.Should().Be("Paul Talker"); // This should fail, typo.
+        }
 
                         
-        //[TestCase(1, Result = "Paul Walker")]
+        [TestCase(1, Result = "Paul Walker")]
         //[TestCase(2, Result = "Cory b")]        // This should fail, typo.
-        //[TestCase(3, Result = "RoyalBaby")]
-        //public string Test_Trending_NUnit(int anIndex)
-        //{
-        //    var result = TrendingRunner.WhatsTrending(anIndex);
-        //    Console.Out.WriteLine("Call \t-> \tresult :\r\n  {0} \t-> \t\"{1}\""
-        //                        , anIndex
-        //                        , result );
-        //    return result;
-        //}
+        [TestCase(3, Result = "RoyalBaby")]
+        public string Test_Trending_NUnit(int anIndex)
+        {
+            var result = TrendingRunner.WhatsTrending(anIndex);
+            Console.Out.WriteLine("Call \t-> \tresult :\r\n  {0} \t-> \t\"{1}\""
+                                , anIndex
+                                , result );
+            return result;
+        }
         
 
     }
