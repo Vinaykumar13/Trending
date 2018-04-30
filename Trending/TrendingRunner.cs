ee2ddbb26fb3464dcd9e7106645ac3bc6e2336c5
Vinaykumar13 <vinaykumarbhandari@gmail.com>
2018-04-30 18:27:50 +0530

    Update TrendingRunner.cs
    Added TODO and HACK for Warning plugins

--- Trending/TrendingRunner.cs
+++ Trending/TrendingRunner.cs
@@ -6,7 +6,7 @@ namespace Trending
     {
         static void Main(string[] args)
         {
-
+        // TODO : Needs to Remove, once Done
             Console.Out.WriteLine("This will show what's trending according to {0}"
                                     , "http://www.google.com.au/trends/topcharts" );
             Console.Out.WriteLine("Hit any key to see the next result.");
@@ -14,10 +14,12 @@ namespace Trending
 
             for (int iteration = 1; iteration < 5; iteration++)
             {
+                // HACK
                 Console.Out.WriteLine("What's trending top list number {0}: {1}."
                                         , iteration
                                         , WhatsTrending(iteration)
                     );
+                // TODO : Needs to Remove, once Done
                 Console.ReadKey(true);
             }
         }
@@ -33,15 +35,18 @@ namespace Trending
             switch (anIndex)
             {
                 case 1:
+                    // TODO : Needs to Remove, once Done
                     answer= "Paul Walker";
                     break;
                 case 2:
+                    // HACK
                     answer= "Cory Monteith";
                     break;
                 case 3:
                     answer= "RoyalBaby";
                     break;
                 default:
+                    // TODO : Needs to Remove, once Done
                     answer = "Choose an index between 1 or 3, or go and search it yourself.";
                     break;
             }
